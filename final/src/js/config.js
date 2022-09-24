ddc9aaaa21bcd1f1ad6518271bae3be5a758ddae
Edgar Andrade <tremsdel@gmail.com>
2022-09-24 09:16:48 -0500

    fixed API Key and built in Parcel

--- final/src/js/config.js
+++ final/src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = 'c4cb320d-3f23-4c60-b1ba-ea3920082faf';
 export const MODAL_CLOSE_SEC = 2.5;
