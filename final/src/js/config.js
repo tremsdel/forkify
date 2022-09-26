0eedf9816b68cfc2c40c98f083f5d212b142633a
Edgar Andrade <tremsdel@gmail.com>
2022-09-26 17:32:32 -0500

    fixed some parcel bugs

--- final/src/js/config.js
+++ final/src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = 'c4cb320d-3f23-4c60-b1ba-ea3920082faf';
+export const KEY = '49b887f4-10d7-47e6-b3f2-e8a87316e42d';
 export const MODAL_CLOSE_SEC = 2.5;
